e363c51b88130ac20c516c1cfcfd0c184a0f2791
orichalque <thiblf@gmail.com>
2016-12-12 13:16:39 +0100

    External application configuration fil eadded

--- src/main/java/configuration/Application.java
+++ src/main/java/configuration/Application.java
@@ -1,8 +1,12 @@
 package configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.PreDestroy;
 
 /**
  * Created by Vandorallen on 12/12/2016.
@@ -10,8 +14,18 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import({ControllerConfiguration.class, RepositoryConfiguration.class})
 public class Application {
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
+
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
+
+    @PreDestroy
+    public void destroy() {
+        SpringApplication.exit(configurableApplicationContext);
+    }
+
+
 }
--- src/main/java/controllers/CharacterController.java
+++ src/main/java/controllers/CharacterController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import interfaces.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +18,12 @@ public class CharacterController {
     @Autowired
     private CharacterRepository characterRepository;
 
+    @Value("${mongo.url}")
+    private String s;
+
     @RequestMapping(value = "/characters")
     public @ResponseBody String getCharacters() {
+        System.out.println(s);
         return characterRepository.getCharacters();
     }
 }
