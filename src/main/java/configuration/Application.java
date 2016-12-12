95a468b030bafd8a800577acf83b2746125b0d37
orichalque <thiblf@gmail.com>
2016-12-12 19:01:49 +0100

    Removing a @preDestroy annotation on the main class

--- src/main/java/configuration/Application.java
+++ src/main/java/configuration/Application.java
@@ -21,11 +21,4 @@ public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
-
-    @PreDestroy
-    public void destroy() {
-        SpringApplication.exit(configurableApplicationContext);
-    }
-
-
 }
