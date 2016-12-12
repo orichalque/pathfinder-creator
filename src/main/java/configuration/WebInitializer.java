3abf5c6c51cd9fa5072bd722675e9c414b07f638
orichalque <thiblf@gmail.com>
2016-12-12 11:51:45 +0100

    MVC Configurer added

--- src/main/java/configuration/WebInitializer.java
+++ src/main/java/configuration/WebInitializer.java
@@ -18,6 +18,6 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
