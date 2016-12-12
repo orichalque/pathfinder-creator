afcc136350fda1aa4d9393e2b433d5520747d15b
orichalque <thiblf@gmail.com>
2016-12-12 12:46:05 +0100

    Add of a procfile for heroku

--- src/main/java/repositories/CharacterRepositoryImpl.java
+++ src/main/java/repositories/CharacterRepositoryImpl.java
@@ -6,6 +6,7 @@ import interfaces.CharacterRepository;
  * Created by Vandorallen on 12/12/2016.
  */
 public class CharacterRepositoryImpl implements CharacterRepository {
+
     @Override
     public String getCharacters() {
         return "characters";
