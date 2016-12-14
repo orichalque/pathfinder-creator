5e38b186faa1548675401664ac7e2c63300c5b0f
orichalque <thiblf@gmail.com>
2016-12-14 15:36:17 +0100

    Species added to the html

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,58 +1,64 @@
-/**
- * Created by Vandorallen on 12/12/2016.
- */
-(function () {
+angular.module("pathCreation", [])
+.controller("charCtrl", ['$scope', '$http', function ($scope, $http) {
 
-    /**
-     * Dependances
-     * $scope: Acces aux variables JS depuis le HTML
-     * $http: Gestion des appels REST
-     * $cookies: Gestion des cookies pour maintenir ma connexion
-     * $window: Redirections
-     * $q promises
-     */
-    var app = angular.module("pathCreation", []);
-    app.controller("charCtrl", ['$scope', '$http', function ($scope, $http) {
+    var species = null;
+    $scope.state = 0;
 
-        $scope.state = 0;
+    $scope.name = "";
+    $scope.age = "";
+    $scope.sex = "assexué";
 
-        $scope.name = "";
-        $scope.age = "";
-        $scope.sex = "assexué";
+    $scope.specieChosen;
 
-        $scope.species = [];
+    $scope.next = function() {
+        console.log($scope.state);
+        $scope.state++;
+    }
 
-        $scope.next = function() {
-            console.log($scope.state);
-            $scope.state++;
-        }
+    $scope.previous = function() {
+        console.log($scope.state);
+        $scope.state--;
+    }
 
-        $scope.previous = function() {
-            console.log($scope.state);
-            $scope.state--;
-        }
+    $scope.getSpecies = function() {
+        $http({
+            method: 'GET',
+            url: "/species"
+        }).then(function successCallback(response) {
+            $scope.species = response.data;
+            species = response.data;
+        }, function errorCallback(response){
+        });
+    };
 
-        $scope.getSpecies = function() {
-            $http({
-                method: 'GET',
-                url: "/species"
-            }).then(function successCallback(response) {
-                $scope.species = response.data;
-            }, function errorCallback(response){
-            });
-        };
+    $scope.pickSpecie = function(species) {
+        $scope.species = species;
+        console.log(specie)
+    };
 
-        $scope.editedItem = {
-            state : false
-        };
+    $scope.editedItem = {
+        state : false
+    };
 
-        $scope.setTrue = function($value) {
-            $value.state = true;
-        };
+    $scope.setTrue = function($value) {
+        $value.state = true;
+    };
 
-        $scope.setFalse = function($value) {
-            $value.state = false;
-        };
+    $scope.setFalse = function($value) {
+        $value.state = false;
+    };
 
-    }]);
-})();
+    $scope.setSpecie = function(specie) {
+        $scope.specieChosen = specie;
+    }
+}])
+.directive('repeater', function() {
+return {
+    restrict: 'E',
+    scope:{
+        specie: "="
+    },
+    link: function($scope, element, attrs) {
+
+    }
+}});
