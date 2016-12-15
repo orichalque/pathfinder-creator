cb6dbf590301f6d65f214521e1680084194b894d
orichalque <thiblf@gmail.com>
2016-12-15 13:25:16 +0100

    Skills added to the database

--- src/main/java/util/JsonBuilder.java
+++ src/main/java/util/JsonBuilder.java
@@ -1,59 +1,64 @@
 package util;
 
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Vandorallen on 15/12/2016.
  */
 public class JsonBuilder {
 
-    private static final List<String> CLASSES = Arrays.asList("Barbare",	"Barde", "Prêtre", "Druide", "Guerrier", "Moine", "Paladin", "Rôdeur", "Roublard", "Ensorceleur", "Magicien");
-    
-    public static void parse() {
+    private static final List<String> CLASSES = Arrays.asList("Barbare", "Barde", "Prêtre", "Druide", "Guerrier", "Moine", "Paladin", "Rôdeur", "Roublard", "Ensorceleur", "Magicien");
+    private static List<String> skills;
+    private static List<String> classes;
 
-        List<String> skills = Arrays.asList("Acrobaties	C	C	—	—	—	C	—	—	C	—	—	Oui	Dex",
-                                            "Art de la magie	—	C	C	C	—	—	C	C	—	C	C	Non	Int",
-                                            "Artisanat	C	C	C	C	C	C	C	C	C	C	C	Oui	Int",
-                                            "Bluff	—	C	—	—	—	—	—	—	C	C	—	Oui	Cha",
-                                            "Connaissances (exploration souterraine)	—	C	—	—	C	—	—	C	C	—	C	Non	Int",
-                                            "Connaissances (folklore local)	—	C	—	—	—	—	—	—	C	—	C	Non	Int",
-                                            "Connaissances (géographie)	—	C	—	C	—	—	—	C	—	—	C	Non	Int",
-                                            "Connaissances (histoire)	—	C	C	—	—	C	—	—	—	—	C	Non	Int",
-                                            "Connaissances (ingénierie)	—	C	—	—	C	—	—	—	—	—	C	Non	Int",
-                                            "Connaissances (mystères)	—	C	C	—	—	—	—	—	—	C	C	Non	Int",
-                                            "Connaissances (nature)	C	C	—	C	—	—	—	C	—	—	C	Non	Int",
-                                            "Connaissances (noblesse)	—	C	C	—	—	—	C	—	—	—	C	Non	Int",
-                                            "Connaissances (plans)	—	C	C	—	—	—	—	—	—	—	C	Non	Int",
-                                            "Connaissances (religion)	—	C	C	—	—	C	C	—	—	—	C	Non	Int",
-                                            "Déguisement	—	C	—	—	—	—	—	—	C	—	—	Oui	Cha",
-                                            "Diplomatie	—	C	C	—	—	—	C	—	C	—	—	Oui	Cha",
-                                            "Discrétion	—	C	—	—	—	C	—	C	C	—	—	Oui	Dex",
-                                            "Dressage	C	—	—	C	C	—	C	C	—	—	—	Non	Cha",
-                                            "Équitation	C	—	—	C	C	C	C	C	—	—	—	Oui	Dex",
-                                            "Escalade	C	C	—	C	C	C	—	C	C	—	—	Oui	For",
-                                            "Escamotage	—	C	—	—	—	—	—	—	C	—	—	Non	Dex",
-                                            "Estimation	—	C	C	—	—	—	—	—	C	C	C	Oui	Int",
-                                            "Évasion	—	C	—	—	—	C	—	—	C	—	—	Oui	Dex",
-                                            "Intimidation	C	C	—	—	C	C	—	C	C	C	—	Oui	Cha",
-                                            "Linguistique	—	C	C	—	—	—	—	—	C	—	C	Non	Int",
-                                            "Natation	C	—	—	C	C	C	—	C	C	—	—	Oui	For",
-                                            "Perception	C	C	—	C	—	C	—	C	C	—	—	Oui	Sag",
-                                            "Premiers secours	—	—	C	C	—	—	C	C	—	—	—	Oui	Sag",
-                                            "Profession	—	C	C	C	C	C	C	C	C	C	C	Non	Sag",
-                                            "Psychologie	—	C	C	—	—	C	—	—	C	—	—	Oui	Sag",
-                                            "Représentation	—	C	—	—	—	C	—	—	C	—	—	Oui	Cha",
-                                            "Sabotage	—	—	—	—	—	—	—	—	C	—	—	Non	Dex",
-                                            "Survie	C	—	—	C	C	—	—	C	—	—	—	Oui	Sag",
-                                            "Utilisation d'objets magiques	—	C	—	—	—	—	—	—	C	C	—	Non	Cha",
-                                            "Vol	—	—	—	C	—	—	—	—	—	C	C	Oui	Dex");
+    public static void main(String[] args) {
+        parse("skills.txt");
+    }
+
+    public static void parse(String fileName) {
+        skills = new ArrayList<>();
+
+        String lineInFile;
+        try (
+                InputStream fis = new FileInputStream("src/main/resources/"+fileName);
+                InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+                BufferedReader br = new BufferedReader(isr)
+        ) {
+            if ((lineInFile = br.readLine()) != null) {
+                //Getting the classes from the first line of the file
+                classes = Arrays.asList(lineInFile.split("\\s"));
+            }
+            while ((lineInFile = br.readLine()) != null) {
+                // Deal with the line
+                skills.add(lineInFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
 
         for (String line : skills){
+            stringBuilder.append("{\n");
             String[] skill = line.split("\\t");
-            System.out.println(Arrays.asList(skill));
-            System.out.println(getClassesFromSkill(skill));
+            stringBuilder.append("\t\"name\" : ").append("\"").append(skill[0]).append("\", \n");
+            stringBuilder.append("\t\"description\" : \"\",\n");
+            stringBuilder.append("\t\"classes\" :").append("[");
+
+
+            for (String clazz : getClassesFromSkill(skill)) {
+                stringBuilder.append("\"").append(clazz).append("\", ");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
+            stringBuilder.append("] \n").append("}\n");
         }
+        System.out.println(stringBuilder.toString());
     }
 
     private static final List<Integer> indexOfCs(String[] skill) {
@@ -69,12 +74,7 @@ public class JsonBuilder {
     }
 
     private static final List<String> getClasses(List<Integer> integerList) {
-        List<String> stringList = new ArrayList<>();
-        for (Integer integer : integerList) {
-            stringList.add(CLASSES.get(integer));
-        }
-
-        return stringList;
+       return integerList.stream().map(classes::get).collect(Collectors.toList());
     }
 
     private static List<String> getClassesFromSkill(String[] skill) {
